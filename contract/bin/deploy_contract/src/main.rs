9d438e1ec7e69e16b0b38ad688c196bb5da74a16
brucelynnzkm <bruce.l@zkm.io>
2023-09-07 19:16:12 +0800

    add the contract part to the read_me

--- contract/bin/deploy_contract/src/main.rs
+++ contract/bin/deploy_contract/src/main.rs
@@ -5,6 +5,6 @@ use deploy_contract::deploy;
 #[tokio::main]
 async fn main() {
 
-    let address = deploy("http://127.0.0.1:8545","/Users/yuqi/project/mips_circuit/contract/verifier/g16/verifier").await;
+    let address = deploy("http://127.0.0.1:8545","/mips_circuit/contract/verifier/g16/verifier").await;
     println!("{:?}",address);
 }
